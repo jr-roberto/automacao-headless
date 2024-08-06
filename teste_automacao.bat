0113caa0c323407682d4376f3534664e806465f0
Roberto Gomes <91014834+jr-roberto@users.noreply.github.com>
2024-08-06 13:55:30 -0300

    Update and rename teste_automacap.bat to teste_automacao.bat

--- teste_automacao.bat
+++ teste_automacao.bat
@@ -6,7 +6,7 @@ REM Ativar o ambiente virtual
 call %VENV_DIR%\env-mypython\Scripts\activate.bat
 
 REM Executar o script Python
-python %VENV_DIR%\teste_automacap.py
+python %VENV_DIR%\teste_automacao.py
 
 REM Desativar o ambiente virtual (opcional)
 call %VENV_DIR%\env-mypython\Scripts\deactivate.bat
